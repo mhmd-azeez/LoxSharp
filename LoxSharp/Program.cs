8cfacac48b13feec62bb6267f94f8b70576117f4
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-02-02 00:08:09 +0300

    Add project files.

---
+++ LoxSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoxSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
