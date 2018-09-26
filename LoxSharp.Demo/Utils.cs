00de1949dd18f693c3eb5d4f31ffbd6c82698b74
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-09-26 13:13:21 +0300

    blazor frontend

---
+++ LoxSharp.Demo/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoxSharp.Demo
+{
+    public static class Utils
+    {
+        public static string Interpret(string source)
+        {
+            try
+            {
+                var output = new StringBuilderOutput();
+                Console.WriteLine(source);
+
+                var scanner = new Scanner(source, output);
+                var tokens = scanner.ScanTokens();
+
+                var parser = new Parser(tokens, output);
+                var statements = parser.Parse();
+
+                var interpreter = new Interpreter(output);
+                interpreter.Interpret(statements);
+
+                return output.ToString();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
